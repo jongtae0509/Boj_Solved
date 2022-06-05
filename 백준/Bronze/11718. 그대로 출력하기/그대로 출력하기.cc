9f7e2a035191224fef6a133260ba01b2597a4b22
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2022-06-05 22:44:05 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    while(1){
+        string s;
+        getline(cin,s);
+        if(s=="") break;
+        cout << s << '\n';
+    }
+
+}
