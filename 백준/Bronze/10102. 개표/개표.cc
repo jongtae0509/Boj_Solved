e4b46cdbcedc572e2f6842b9378d780e55c39832
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2022-06-23 16:13:03 +0900

    [Bronze II] Title: 개표, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    string s; cin >> s;
+    int a=0,b=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='A') a++;
+        else b++;
+    }
+    if(a>b) cout << "A";
+    else if(a==b) cout << "Tie";
+    else cout << "B";
+}
