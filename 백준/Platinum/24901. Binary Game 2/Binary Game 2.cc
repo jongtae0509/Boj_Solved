726e6b0dbf02e9c2ad9a6a5c999ec821ca36c055
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2022-06-28 00:28:05 +0900

    [Platinum I] Title: Binary Game 2, Time: 212 ms, Memory: 98412 KB -BaekjoonHub

---
+++ 백준/Platinum/24901. Binary Game 2/Binary Game 2.cc
@@ -0,0 +1,50 @@
+//;#\u000a import java.util.*;public class Main{public static void main(String args[]){Scanner sc=new Scanner(System.in);int n;n= sc.nextInt();if(n==0)System.out.println("0");else if(n==1)System.out.println("01");else if(n==2)System.out.println("0110");else if(n==3) System.out.println("011011");else if(n==4)System.out.println("011011100");else if(n==5)System.out.println("011011100101");else if(n==6) System.out.println("011011100101110");else if(n==7)System.out.println("011011100101110111");else if(n==8) System.out.println("0110111001011101111000");else if(n==9)System.out.println("01101110010111011110001001");else System.out.println("011011100101110111100010011010");}}/*
+#if true /*
+n = gets.to_i
+if n == 0
+	puts "0"
+elsif n == 1
+	puts "01"
+elsif n == 2
+	puts "0110"
+elsif n == 3
+	puts "011011"
+elsif n == 4
+	puts "011011100"
+elsif n == 5
+	puts "011011100101"
+elsif n == 6
+	puts "011011100101110"
+elsif n == 7
+	puts "011011100101110111"
+elsif n == 8
+	puts "0110111001011101111000"
+elsif n == 9
+	puts "01101110010111011110001001"
+else
+	puts "011011100101110111100010011010"
+end
+=begin
+/**///\u000a/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n; cin >> n;
+if(n==0) cout << "0";
+else if(n==1) cout << "01";
+else if(n==2) cout << "0110";
+else if(n==3) cout << "011011";
+else if(n==4) cout << "011011100";
+else if(n==5) cout << "011011100101";
+else if(n==6) cout << "011011100101110";
+else if(n==7) cout << "011011100101110111";
+else if(n==8) cout << "0110111001011101111000";
+else if(n==9) cout << "01101110010111011110001001";
+else cout << "011011100101110111100010011010";
+}
+// */
+// */ import Foundation; let line = readLine()!; let lineArr = line.components(separatedBy: " "); let n = Int(lineArr[0])!; if(n == 0) {print("0");}; if(n == 1) {print("01");}; if(n == 2) {print("0110");}; if(n == 3) {print("011011");}; if(n == 4) {print("011011100");}; if(n == 5) {print("011011100101");}; if(n == 6) {print("011011100101110");}; if(n == 7) {print("011011100101110111");}; if(n == 8) {print("0110111001011101111000");}; if(n == 9) {print("01101110010111011110001001");}; if(n == 10) {print("011011100101110111100010011010");} ;// \u000a /*
+#endif
+/*
+=end
+# */
