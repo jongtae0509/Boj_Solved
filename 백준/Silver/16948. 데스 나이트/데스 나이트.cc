bc17e6b77f22a0d1d51e87b48fbafa7d6f4304a3
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2022-06-28 23:13:14 +0900

    [Silver I] Title: 데스 나이트, Time: 0 ms, Memory: 2340 KB -BaekjoonHub

---
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.cc
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define X first
+#define Y second
+
+using namespace std;
+int dx[6] = {-2,-2,0,0,2,2};
+int dy[6] = {-1,1,-2,2,-1,1};
+
+int main(){
+    int map[300][300] = {};
+    bool vis[300][300] = {};
+    queue<pair<int,int>> q;
+    int l; cin >> l;
+    pair<int,int> st; cin >> st.X >> st.Y; q.push({st.X,st.Y}); vis[st.X][st.Y] = 1;
+    pair<int,int> des; cin >> des.X >> des.Y;
+    bool ch=0;
+    int ans=0;
+    while(!q.empty()&&!ch){
+        auto cur = q.front(); q.pop();
+        for(int dir=0;dir<6;dir++){
+            int nx = dx[dir] + cur.X;
+            int ny = dy[dir] + cur.Y;
+            if(nx<0||nx>=l||ny<0||ny>=l) continue;
+            if(vis[nx][ny]||map[nx][ny]!=0) continue;
+            if(nx==des.X&&ny==des.Y){
+                ch = 1;
+                ans = map[cur.X][cur.Y]+1;
+                break;
+            }
+            map[nx][ny] = map[cur.X][cur.Y]+1;
+            q.push({nx,ny});
+            vis[nx][ny] = 1;
+        }
+    }
+//    for(int i=0;i<l;i++){
+//        for(int j=0;j<l;j++){
+//            cout << map[i][j] << ' ';
+//        }
+//        cout << '\n';
+//    }
+    if(!ch) ans = -1;
+    cout << ans << '\n';
+}
