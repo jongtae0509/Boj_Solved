f5be16696b46ea8503e765ae6d3c734f1b1f0870
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-04-11 22:57:07 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char s[100]; cin >> s;
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n",s);
+    printf(":fan::fan::fan:");
+}
