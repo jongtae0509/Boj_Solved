05100cea2ab55ac638bbe63eeae11430982e4018
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-04-15 01:05:34 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    if(n == 1)
+       cout<<"Leading the Way to the Future";
+    else
+        cout<<"YONSEI";
+
+}
